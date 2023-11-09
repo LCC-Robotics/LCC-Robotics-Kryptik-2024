15692a250b9739f0f9e81cdee09159b6913060a0
Kenneth Lew <lew1101@192.168.12.23>
2023-11-08 20:48:17 -0500

    update CrcLib to 1.4.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include <Arduino.h>
-#include <CrcLib.h> // https://robocrc.atlassian.net/wiki/spaces/AR/pages/403767325/CrcLib+Functions+-+An+overview
+#include <CrcLib.h> // v1.4.0
 #include <Encoder.h>
 
-using Crc::CrcLib;
+// CrcLib Reference:
+// https://robocrc.atlassian.net/wiki/spaces/AR/pages/403767325/CrcLib+Functions+-+An+overview
+
 using CrcUtility::ANALOG;
 using CrcUtility::BUTTON;
 
 // ========================
-// PINS
-// ========================
+// PINS:
 
 constexpr uint8_t FL_DRIVE_PIN = CRC_PWM_1;
 constexpr uint8_t BL_DRIVE_PIN = CRC_PWM_2;
@@ -16,8 +17,7 @@ constexpr uint8_t BR_DRIVE_PIN = CRC_PWM_3;
 constexpr uint8_t FR_DRIVE_PIN = CRC_PWM_4;
 
 // ========================
-// ANALOG INPUTS
-// ========================
+// ANALOG INPUTS:
 
 constexpr ANALOG FORWARD_CHANNEL = ANALOG::JOYSTICK1_Y;
 constexpr ANALOG YAW_CHANNEL = ANALOG::JOYSTICK1_X;
@@ -26,16 +26,14 @@ constexpr ANALOG STRAFE_CHANNEL = ANALOG::JOYSTICK2_X;
 constexpr BUTTON PRECISION_CONTROL = BUTTON::L1;
 
 // ========================
-// DIGITAL INPUTS
-// ========================
+// DIGITAL INPUTS:
 
 constexpr BUTTON DIE_BUTTON = BUTTON::LOGO;
 
 // ========================
-// CONST
-// ========================
+// CONST VARS:
 
-constexpr uint8_t DIVISION_FACTOR = 2;
+constexpr uint8_t PRECISION_DIVISION_FACTOR = 2;
 
 void setup()
 {
@@ -66,15 +64,19 @@ void loop()
         return die();
     }
 
+    // Precision Control:
+
     int8_t foward_val = CrcLib::ReadAnalogChannel(FORWARD_CHANNEL);
     int8_t yaw_val = CrcLib::ReadAnalogChannel(YAW_CHANNEL);
     int8_t strafe_val = CrcLib::ReadAnalogChannel(STRAFE_CHANNEL);
 
     if (CrcLib::ReadDigitalChannel(PRECISION_CONTROL)) {
-        foward_val /= DIVISION_FACTOR;
-        yaw_val /= DIVISION_FACTOR;
-        strafe_val /= DIVISION_FACTOR;
+        foward_val /= PRECISION_DIVISION_FACTOR;
+        yaw_val /= PRECISION_DIVISION_FACTOR;
+        strafe_val /= PRECISION_DIVISION_FACTOR;
     }
+
+    // Drive:
 
     CrcLib::MoveHolonomic(
         foward_val, yaw_val, strafe_val,
